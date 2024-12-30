fca5ee0dbb68451a39ff6b09cb61248ea280975c
Teetad <teetadraseesit@gmail.com>
2024-12-30 20:45:06 +0700

    lab13_2 done

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,28 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool image [N][M]){
+    cout << "------------------------------------------------------------------------" << endl;
+   for (int j = 0; j < N; j++){
+        for (int i = 0; i < M; i++){
+            if (i%70 == 0){
+                cout << "|";
+            }
+            if (image[j][i] == 1) cout << "*";
+            else cout << " ";
+        }
+        cout << "|" << endl;
+    }
+    cout << "------------------------------------------------------------------------" << endl;
+}
+
+void updateImage(bool image[N][M],int s ,int x, int y){
+    for (int j = 0; j < 70; j++){
+        for (int i = 0; i < 30; i++){
+            if (sqrt(pow(i-x,2) + pow(j-y,2)) <= s-1){
+                image[i][j] = 1;
+            }
+        }
+    }
+}
